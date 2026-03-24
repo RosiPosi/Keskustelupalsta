69e74095151552538fd5fc8ec8940fea2e9d5e4b
RosiPosi <116714787+RosiPosi@users.noreply.github.com>
2026-03-24 13:10:04 +0200

    user's post

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def index():
 def new_item():
     return render_template("new_item.html")
 
+@app.route("/create_item")
+def create_item():
+    title = request.form["title"]
+    description = request.form["description"]
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -47,10 +52,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -58,5 +66,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
