d74f5da71b9066a34b27cca1102935b371352996
RosiPosi <116714787+RosiPosi@users.noreply.github.com>
2026-04-13 21:07:38 +0300

    user posts

--- users.py
+++ users.py
@@ -8,10 +8,11 @@ def get_user(user_id):
     return result[0] if result else None
 
 def get_item(user_id):
-    sql = """SELECT id, username
-            FROM users
-            WHERE id = ?"""
-    result = db.query(sql, [user_id])
-    return result[0] if result else None
+    sql = """SELECT id, title
+            FROM items
+            WHERE user_id = ?
+            ORDER BY id DESC"""
+    return db.query(sql, [user_id])
+
 
 
